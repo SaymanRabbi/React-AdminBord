3769e70f28716833a148fbeb36a9d7ffe6ed8f95
saymanrabbi <mdrabbi.asm@gmail.com>
2022-11-16 16:09:15 +0600

    widgest setup

--- src/Components/widgets/Widgest.jsx
+++ src/Components/widgets/Widgest.jsx
@@ -1,13 +1,20 @@
+import { KeyboardArrowUp, PersonOutlined } from '@mui/icons-material';
 import React from 'react';
 import './Widgest.scss';
 const Widgest = () => {
     return (
         <div className='widgest'>
              <div className="left">
-                left
+               <span className='tittle'>COUNTREY</span>
+               <span className='Counter'>100000</span>
+               <span className='link'>See All Country</span>
              </div>
              <div className="right">
-                right
+                <div className='percentage positive nagitive'>
+                    <KeyboardArrowUp/>
+                    20%
+                </div>
+                <PersonOutlined className='icon'/>
              </div>
         </div>
     );
