9699a124344a328cd6d22ee0ade652b7409ac909
saymanrabbi <mdrabbi.asm@gmail.com>
2022-11-16 12:24:40 +0600

    add icon

--- src/Components/sidebar/Sidebar.jsx
+++ src/Components/sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
+import { CalendarMonth, ChangeCircle, Compare, Flag, LocationCity, SouthAmerica, Speed, Topic } from '@mui/icons-material';
+import DashboardIcon from '@mui/icons-material/Dashboard';
 import React from 'react';
 import './Sidebar.scss';
-
 const Sidebar = () => {
     return (
         <div className='sidebar'>
@@ -11,10 +12,42 @@ const Sidebar = () => {
             <hr />
             <div className='center'>
                 <ul>
-                    <li><span>Dashboard</span></li>
-                    <li><span>Dashboard</span></li>
-                    <li><span>Dashboard</span></li>
-                    <li><span>Dashboard</span></li>
+                    <li>
+                        <DashboardIcon/>
+                        <span>Dashboard</span>
+                    </li>
+                    <li>
+                        <Speed/>
+                        <span>Intensity</span>
+                    </li>
+                    <li>
+                        <ChangeCircle/>
+                        <span>Likelihood</span>
+                    </li>
+                    <li>
+                        <Compare/>
+                        <span>Relevance</span>
+                    </li>
+                    <li>
+                        <CalendarMonth/>
+                        <span>Year</span>
+                    </li>
+                    <li>
+                        <Flag/>
+                        <span>Country</span>
+                    </li>
+                    <li>
+                        <Topic/>
+                        <span>Topics</span>
+                    </li>
+                    <li>
+                        <SouthAmerica/>
+                        <span>Region</span>
+                    </li>
+                    <li>
+                        <LocationCity/>
+                        <span>City</span>
+                    </li>
                 </ul>
             </div>
             <div className='bottom'>Color Option</div>
