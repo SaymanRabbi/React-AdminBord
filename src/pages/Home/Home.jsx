0a099367f607dd009640ca870ea6867daee21ce0
saymanrabbi <mdrabbi.asm@gmail.com>
2022-11-16 17:20:19 +0600

    get all data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import axios from "axios";
+import { useQuery } from 'react-query';
 import Navbar from '../../Components/Navbar/Navbar';
 import Sidebar from '../../Components/sidebar/Sidebar';
 import Widgest from '../../Components/widgets/Widgest';
 import './Home.scss';
 const Home = () => {
+    const fetchUser = async () => {
+        const response = await axios.get(
+          "http://localhost:5000/api/v1/getAlldata"
+        );
+        return response.data;
+      };
+    const {data:alldata,isLoading,error} = useQuery('user',fetchUser)
+    if(isLoading){
+        return <h1>Loading...</h1>
+    }
+    console.log(alldata);
     return (
         <div className='home'>
             <Sidebar/>
