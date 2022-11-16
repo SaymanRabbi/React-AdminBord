de657913f4244753d16f4e156554b818339c33b1
saymanrabbi <mdrabbi.asm@gmail.com>
2022-11-16 10:50:20 +0600

    setup router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
